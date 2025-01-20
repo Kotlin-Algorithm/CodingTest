9044c4cecc7b0ad1609bb9c84061a9e2bcd03ba4
권동휘 <ARProxy>
2025-01-20 23:40:55 +0900

    🔥 코드 리뷰 하자~

--- Donghwi/src/backjoon/B11724.kt
+++
@@ -1,9 +0,0 @@
-package backjoon
-
-class B11724 {
-    fun connection() {
-        val (n, m) = readln().split(" ").map(String::toInt)
-        val node = Array(m) { Array(n) { 0 } }
-        println(node.contentToString())
-    }
-}
