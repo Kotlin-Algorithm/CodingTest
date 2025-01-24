01d2b6ba9cdceed103f0fd2dcb30bf537217b4a2
leesoobeen <02ggang9@gmail.com>
2025-01-24 21:43:33 +0900

    fix: 3주차 백준 10917번 문제 자료구조 수정 및 최적화

--- Soobeen/src/week3/BaekJoon10917.kt
+++ Soobeen/src/week3/BaekJoon10917.kt
@@ -1,30 +1,31 @@
 package week3
 
 import java.util.*
+import java.util.ArrayDeque
 
 fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
-    val queue: Queue<Int> = LinkedList()
+    val queue: Queue<Int> = ArrayDeque()
     val distance = Array(n + 1) { -1 }
-    val matrix = mutableMapOf<Int, MutableList<Int>>()
+    val matrix = Array(n + 1) { mutableListOf<Int>() }
 
     repeat(m) {
         val (start, end) = readln().split(" ").map { it.toInt() }
-        matrix[start]?.add(end) ?: matrix.put(start, mutableListOf(end))
+        matrix[start].add(end)
     }
 
     distance[1] = 0
     queue.add(1)
 
     while (queue.isNotEmpty()) {
-        val value = queue.poll()
-        matrix[value]
-            ?.filter { distance[it] == -1 }
-            ?.forEach {
-                distance[it] = distance[value] + 1
-                queue.add(it)
+        val currentNode = queue.poll()
+        val list = matrix[currentNode]
+        for (index in list) {
+            if (distance[index] == -1) {
+                distance[index] = distance[currentNode] + 1
+                queue.add(index)
             }
-            ?: continue
+        }
     }
 
     println(distance[n])
