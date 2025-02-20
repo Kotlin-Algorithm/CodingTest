3edd900a0aab73ca521fb8caf9ecceaa2d680ec2
Ga0Lee <0419rkdid@naver.com>
2025-02-20 21:52:37 +0900

    feat: Baekjoon 1940 문제 풀이

---
+++ Gayeong/src/week7/BaekJoon1940.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1940 {
+    static int N, M;
+    static int numbers[];
+
+    static int twoPointer() {
+        Arrays.sort(numbers);
+        int left = 0, right = N - 1, count = 0;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == M) {
+                count++;
+                left++;
+                right++;
+            } else if (sum < M) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++)
+            numbers[i] = Integer.parseInt(st.nextToken());
+
+        System.out.println(twoPointer());
+    }
+}
